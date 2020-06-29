51b1df837d34b6de246c239e3235acbc8c855faf
Oz <oharrison1998@outlook.com>
2020-06-29 21:17:44 +0100

    Initialised project, established react front end and express back end.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # game-book-creator
-A web app that allows the creation and playing of game book adventures, based on series like Fighting Fantasy
+A web app that allows the creation and playing of game book adventures, based on series like Fighting Fantasy.
---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>{this.state.apiResponse}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
